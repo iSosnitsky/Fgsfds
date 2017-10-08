181ed2333996ab9d7bfd0aba775dd4878ff3c071
iSosnitsky <spiritblossom@icloud.com>
2017-10-08 03:17:31 +0300

    Credit card check now uses stream

--- src/main/java/Fgsfds/cardCheck/CreditCard.java
+++ src/main/java/Fgsfds/cardCheck/CreditCard.java
@@ -1,5 +1,7 @@
 package Fgsfds.cardCheck;
 
+import java.util.stream.IntStream;
+
 //Could also be used as a debit card
 public class CreditCard {
     private String cardNumber;
@@ -12,15 +14,21 @@ public class CreditCard {
     public Boolean checkTheCard(){
 
         int[] cardDigits = this.cardNumber.chars().map(Character::getNumericValue).toArray();
-        int sum = 0;
-        for (int i = cardDigits.length-1; i>=0; i--){
-
-            if(((i-cardDigits.length)%2)==0){
-                sum+= ((cardDigits[i]*2)<=9) ? (cardDigits[i]*2) : (((cardDigits[i]*2)%10)+(int)((cardDigits[i]*2)/10));
-            } else {
-                sum+= cardDigits[i];
-            }
-        }
+        //I didn't like this solution at first
+        //But when it worked - i started to dislike it even more
+        final int sum = IntStream.range(0, cardDigits.length)
+                .map(i -> cardDigits.length - i - 1)
+                .map(i -> (i % 2 == 0) ? (cardDigits[i] * 2) : (cardDigits[i]))
+                .map(i -> (i >= 9) ? (i % 10) + (i / 10) : i)
+                .sum();
+//        for (int i = cardDigits.length-1; i>=0; i--){
+//
+//            if(((i-cardDigits.length)%2)==0){
+//                sum+= ((cardDigits[i]*2)<=9) ? (cardDigits[i]*2) : (((cardDigits[i]*2)%10)+(int)((cardDigits[i]*2)/10));
+//            } else {
+//                sum+= cardDigits[i];
+//            }
+//        }
         return (sum%10==0);
     }
 }
