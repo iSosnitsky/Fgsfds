ece5635ff9d5e316ec21731f9189fa5cd6bdc1e7
iSosnitsky <spiritblossom@icloud.com>
2017-10-09 23:27:14 +0300

    MapElement is now final and package-private

--- src/main/java/Fgsfds/map/MapElement.java
+++ src/main/java/Fgsfds/map/MapElement.java
@@ -1,20 +1,20 @@
 package Fgsfds.map;
 
 
-public class MapElement<K, V> {
+final class MapElement<K, V> {
     private final K key;
     private final V value;
 
-    public MapElement(K key, V value) {
+    MapElement(K key, V value) {
         this.key = key;
         this.value = value;
     }
 
-    public V getValue() {
+    V getValue() {
         return value;
     }
 
-    public K getKey() {
+    K getKey() {
         return key;
     }
 }
